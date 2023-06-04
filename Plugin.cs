e3e413e4fa2fa5087f5fb403719e9a98e35dfe73
AkjaHAsLk1IALk0MasH <46046453+AgaSpace@users.noreply.github.com>
2023-06-04 11:39:55 +0700

    Added a search for all files

--- Plugin.cs
+++ Plugin.cs
@@ -13,10 +13,16 @@ public class ReferencesPlugin : TerrariaPlugin
     {
         AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
         Directory.CreateDirectory(ReferencesDirectoryPath);
+
+        files = Directory.GetFiles(ReferencesDirectoryPath, "*.dll", SearchOption.AllDirectories)
+            .ToDictionary(i => Path.GetFileName(i), i => i);
+
         ServerApi.LogWriter.PluginWriteLine(this, "The references have been successfully uploaded.", TraceLevel.Info);
     }
      
     private Dictionary<string, Assembly> loadedAssemblies = new Dictionary<string, Assembly>();
+    private Dictionary<string, string> files = new Dictionary<string, string>();
+
     public static readonly string ReferencesDirectoryPath 
         = Path.Combine(AppContext.BaseDirectory, "References");
 
@@ -27,23 +33,28 @@ public class ReferencesPlugin : TerrariaPlugin
     private Assembly? CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
     {
         string text = args.Name.Split(',')[0];
-        string path = Path.Combine(ReferencesDirectoryPath, text + ".dll");
-        try
+        string name = text + ".dll";
+
+        if (files.TryGetValue(name, out string path))
         {
-            if (File.Exists(path))
+            try
             {
-                if (!loadedAssemblies.TryGetValue(path, out Assembly? assembly))
+                if (File.Exists(path))
                 {
-                    assembly = Assembly.Load(File.ReadAllBytes(path));
-                    loadedAssemblies.Add(path, assembly);
+                    if (!loadedAssemblies.TryGetValue(path, out Assembly? assembly))
+                    {
+                        assembly = Assembly.Load(File.ReadAllBytes(path));
+                        loadedAssemblies.Add(path, assembly);
+                    }
+                    return assembly;
                 }
-                return assembly;
+            }
+            catch (Exception ex)
+            {
+                ServerApi.LogWriter.PluginWriteLine(this, string.Format("Error on resolving assembly \"{0}.dll\":\n{1}", text, ex), TraceLevel.Error);
             }
         }
-        catch (Exception ex)
-        {
-            ServerApi.LogWriter.PluginWriteLine(this, string.Format("Error on resolving assembly \"{0}.dll\":\n{1}", text, ex), TraceLevel.Error);
-        }
+        
         return null;
     }
 }
